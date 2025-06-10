edbd9b1a131b1c34a6862d22bf5d43cb59537835
Sunwoo Moon <67269487+MswTester@users.noreply.github.com>
2025-06-11 08:06:48 +0900

    Translate README to English and add basic tests

---
+++ src/tests/framework.test.ts
@@ -0,0 +1,28 @@
+import { createElement, Fragment, mount, Component, Render, State, Event } from 'echelon';
+
+@Component('div')
+class Counter {
+  @State() count = 0;
+
+  @Event('click')
+  increment(_e: Event) {
+    this.count++;
+  }
+
+  @Render()
+  render() {
+    return createElement('span', null, this.count);
+  }
+}
+
+describe('Echelon basic component', () => {
+  test('state updates trigger re-render', () => {
+    const container = document.createElement('div');
+    mount(createElement(Counter, null), container);
+    expect(container.innerHTML).toBe('<div><span>0</span></div>');
+
+    // dispatch click event to increment count
+    container.firstElementChild!.dispatchEvent(new MouseEvent('click'));
+    expect(container.innerHTML).toBe('<div><span>1</span></div>');
+  });
+});
